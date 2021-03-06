44285ec85df5c2865904d70d5699ff5b5a4d99d4
hallen013 <hallen013@gmail.com>
2021-03-06 16:51:52 -0300

    3rd code

---
+++ Python/exercise2.py
@@ -0,0 +1,6 @@
+print('Digite a seguir o dia, mês e ano do seu nascimento.')
+day=input('Dia: ')
+month=input('Mês: ')
+year=input('Ano: ')
+
+print('Você nasceu no dia {}/{}/{}.'.format(day,month,year))
