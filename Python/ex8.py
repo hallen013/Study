68dadcecc02c36568b8d42fa7fddf6aeafe81cc9
hallen013 <hallen013@gmail.com>
2021-03-08 18:21:18 -0300

    +clean

--- Python/ex8.py
+++ Python/ex8.py
@@ -1,5 +1,5 @@
 print('Conversão mts para cm/mm')
 n1 = float(input('Digite o valor para conversão: '))
-cm = int(n1 * (10**2))
-mm = int(n1 * (10**3))
+cm = n1 * 100
+mm = n1 * 1000
 print('{} mts é equivalente a:\n{} cm\n{} mm'.format(n1, cm, mm))
