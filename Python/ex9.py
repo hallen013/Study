8edb97f71692e30055c6ce98235cf9f329ea6e84
hallen013 <hallen013@gmail.com>
2021-03-08 16:03:34 -0300

    ex9

---
+++ Python/ex9.py
@@ -0,0 +1,13 @@
+print('Tabuada')
+n1 = int(input('Digite um número: '))
+a = n1 * 1
+b = n1 * 2
+c = n1 * 3
+d = n1 * 4
+e = n1 * 5
+f = n1 * 6
+g = n1 * 7
+h = n1 * 8
+i = n1 * 9
+j = n1 * 10
+print('{0} x 1 = {1}\n{0} x 2 = {2}\n{0} x 3 = {3}\n{0} x 4 = {4}\n{0} x 5 = {5}\n{0} x 6 = {6}\n{0} x 7 = {7}\n{0} x 8 = {8}\n{0} x 9 = {9}\n{0} x 10 = {10}'.format(n1, a, b, c, d, e, f, g, h, i, j))
