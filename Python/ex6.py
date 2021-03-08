33ac63cd27f96d97f1b04ab646aa085423bdd476
hallen013 <hallen013@gmail.com>
2021-03-08 14:19:53 -0300

    +clean

--- Python/ex6.py
+++ Python/ex6.py
@@ -2,4 +2,4 @@ n1 = int(input('Digite um número: '))
 d = n1*2
 t = n1*3
 r = n1**(1/2)
-print('Número = {}\nDobro = {}\nTriplo = {}\nRaiz quadrada = {:.3f}'.format(n1, d ,t ,r))
+print('Número = {}\nDobro = {}\nTriplo = {}\nRaiz quadrada = {:.3f}'.format(n1, d, t, r))
