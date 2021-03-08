890456726612b50e26a36629d545a33adc3e30a1
hallen013 <hallen013@gmail.com>
2021-03-08 18:41:50 -0300

    +clean

--- Python/ex12.py
+++ Python/ex12.py
@@ -1,6 +1,5 @@
 print('Cálculo de descontos')
 # Valor do desconto = 5%
 vlr = float(input('Digite o valor a ser calculado: R$ '))
-desc = vlr * (5/100)
-prc = vlr - desc
+prc = vlr - (vlr * (5/100))
 print('O desconto de 5% do valor R$ {:.2f} = R$ {:.2f}'.format(vlr, prc))
